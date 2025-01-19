2cec5c774b24692d4894547f37d3a6f317316730
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-01-19 10:18:23 +0530

    Renamed file 1 to _1_DataTypes.java

--- Step1_Basics/Lec1/_1_DataTypes.java
+++ Step1_Basics/Lec1/_1_DataTypes.java
@@ -1,7 +1,7 @@
 package Lec1;
 //link: https://www.geeksforgeeks.org/problems/data-type-1666706751/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=data-type
 
-public class DataTypes {
+public class _1_DataTypes {
     static int dataTypeSize(String str){
         int value = 0;
         switch(str){
