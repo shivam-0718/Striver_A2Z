437398c4b465129afaaf0764e8023b4454adb9cf
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-01-19 10:17:05 +0530

    Moved file to Lec 1 folder

--- Step1_Basics/Lec1/DataTypes.java
+++ Step1_Basics/Lec1/DataTypes.java
@@ -1,3 +1,4 @@
+package Lec1;
 //link: https://www.geeksforgeeks.org/problems/data-type-1666706751/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=data-type
 
 public class DataTypes {
