1ea1614167bbc8641d2000ecd62db4fd1b048ddf
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-01-19 10:19:46 +0530

    Solved Step 1 Lec 1.3 problem Conditionals.java

---
+++ Step1_Basics/Lec1/_2_Conditionals.java
@@ -0,0 +1,17 @@
+package Lec1;
+
+public class _2_Conditionals {
+    class Solution {
+        public static String compareNM(int n, int m) {
+            if (n < m) {
+                return "lesser";
+            } else if (n > m) {
+                return "greater";
+            } else {
+                return "equal";
+            }
+        }
+    }
+}
+
+//link: https://www.geeksforgeeks.org/problems/java-if-else-decision-making0924/0?category%255B%255D=Java&difficulty%255B%255D=-2&page=1&query=category%255B%255DJavadifficulty%255B%255D-2page1category%255B%255DJava
