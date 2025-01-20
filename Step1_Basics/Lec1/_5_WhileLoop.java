da0d143b70a8736e323388e193a4b9a83a489f9a
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-01-20 20:04:50 +0530

    Solved Step 1 Lec 1.7 problem While Loop.java

---
+++ Step1_Basics/Lec1/_5_WhileLoop.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class _5_WhileLoop{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number whose table you want: ");
+        int n = sc.nextInt();
+        int i = 10;
+        while (i > 0){
+            System.out.print((n * i) + " ");
+            i--;
+        }
+
+        //using for loop
+        for (int j = 10; j >= 1; j--){
+            System.out.print((n * j) + " ");
+        }
+        System.out.println();
+    }
+}
+//link: https://www.geeksforgeeks.org/problems/while-loop-printtable-java/1
