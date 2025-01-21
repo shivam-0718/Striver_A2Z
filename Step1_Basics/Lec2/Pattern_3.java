646fa19e06b26ff25649be536a7eb44a182732c2
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-01-21 23:14:02 +0530

    Solved Step 1 Lec 2.3 problem

---
+++ Step1_Basics/Lec2/Pattern_3.java
@@ -0,0 +1,12 @@
+package Lec2;
+
+public class Pattern_3 {
+    public static void main(String[] args) {
+        for(int i = 1; i<=5; i++){
+            for(int j = 1; j<=i; j++){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
