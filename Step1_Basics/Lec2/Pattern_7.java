b11c9a98d0cacc270d13e306e1c01a55ba421ff9
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-01-23 22:26:20 +0530

    New logic implementation

--- Step1_Basics/Lec2/Pattern_7.java
+++ Step1_Basics/Lec2/Pattern_7.java
@@ -3,8 +3,8 @@ package Lec2;
 public class Pattern_7 {
     public static void main(String[] args) {
         int n = 5;
-        for (int i = n; i >= 1; i--) {
-            for (int j = 1; j <= i; j++) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n + 1 - i; j++) {
                 System.out.print("*");
             }
             System.out.println();
