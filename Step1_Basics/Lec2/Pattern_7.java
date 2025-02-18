dbc1d2ff20b63485b3556ee26682f00ab3442c42
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-02-18 07:19:11 +0530

    Added another static method as a practice

--- Step1_Basics/Lec2/Pattern_7.java
+++ Step1_Basics/Lec2/Pattern_7.java
@@ -3,28 +3,51 @@ package Lec2;
 import java.util.Scanner;
 
 public class Pattern_7 {
+
+    public static void pattern(int n) {
+        for (int i = 1; i <= n; i++) {
+            // space
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            //stars
+            for (int j = 1; j <= (2*i)-1; j++) {
+                System.out.print("*");
+            }
+
+            // space
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt(); // taking input as n
-        int final_col = (2 * n) - 1; //calculating final columns based on input
+        int n = sc.nextInt(); // taking input as n -> number of rows
+        int final_col = (2 * n) - 1; // calculating final columns based on input
 
         for (int i = 1; i <= n; i++) {
-            int current_col = (2*i) - 1; //current columns where * will be printed
-            int spaces = (final_col - current_col) / 2; //current columns where " " will be printed
-            
-            //printing spaces
+            int current_col = (2 * i) - 1; // current columns where * will be printed
+            int spaces = (final_col - current_col) / 2; // current columns where " " will be printed
+
+            // printing spaces
             for (int j = 1; j <= spaces; j++) {
                 System.out.print(" ");
             }
 
-            //printing *
+            // printing *
             for (int j = 1; j <= current_col; j++) {
                 System.out.print("*");
             }
 
-            //printing new line after printing * and " "
+            // printing new line after printing * and " "
             System.out.println();
         }
+
+        pattern(5);
 
         sc.close();
     }
