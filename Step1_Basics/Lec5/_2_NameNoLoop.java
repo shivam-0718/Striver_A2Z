053ae0ea6a7168a7890ce6c5812ed6afa7a5393a
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-03-01 13:16:57 +0530

    Added link for Step 1 Lec5.2 Problem

--- Step1_Basics/Lec5/_2_NameNoLoop.java
+++ Step1_Basics/Lec5/_2_NameNoLoop.java
@@ -1,6 +1,8 @@
 package Lec5;
 
 public class _2_NameNoLoop {
+    //link: https://www.geeksforgeeks.org/problems/print-gfg-n-times/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=print-gfg-n-times
+
     public static void printNames(int n){
         if(n == 0){
             return;
@@ -9,6 +11,6 @@ public class _2_NameNoLoop {
         printNames(n-1);
     }
     public static void main(String[] args) {
-
+        printNames(10);
     }
 }
