8b14a4a16fb78e5e050b5382b4a7afdc105a2026
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-03-02 21:49:17 +0530

    Solved Step 1 Lec 5.9 Fibonacci with recursion

---
+++ Step1_Basics/Lec5/_9_FibonacciNumber.java
@@ -0,0 +1,52 @@
+package Lec5;
+
+public class _9_FibonacciNumber {
+
+    //logic: fib(i) = fib(i - 1) + fib(i - 2)
+    public static void fibonacciNo(int N){
+        int[] series = new int[N+1];
+        if(N == 0){
+            System.out.println(0);
+        }
+        series[0] = 0;
+        series[1] = 1;
+        for (int i = 2; i < series.length; i++) {
+            series[i] = series[i-1] + series[i-2];
+        }
+        System.out.println("Printing the fibonacci series");
+        for(int i : series){
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    //using recursion
+    public static int fibonacci(int n){
+        if(n <= 1){
+            return n;
+        }
+        return (fibonacci(n-1) + fibonacci(n-2));
+    }
+
+    //using iteration (Good approach)
+    public static int fibo(int n){
+        if(n <= 1){
+            return n;
+        }
+        int sum = 0;
+        int last = 0, slast = 1;
+        while (n -- > 1){
+            sum = last + slast;
+            last = slast;
+            slast = sum;
+        }
+        return slast;
+    }
+
+
+    public static void main(String[] args) {
+        fibonacci(5)
+        fib;
+
+    }
+}
