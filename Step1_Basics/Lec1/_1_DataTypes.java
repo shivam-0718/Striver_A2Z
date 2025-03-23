91b1dc8aa906c68d1f36e9965327cabe345836d8
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-03-23 19:43:56 +0530

    Step 1 Lec 1.2 code modify

--- Step1_Basics/Lec1/_1_DataTypes.java
+++ Step1_Basics/Lec1/_1_DataTypes.java
@@ -2,33 +2,21 @@ package Lec1;
 //link: https://www.geeksforgeeks.org/problems/data-type-1666706751/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=data-type
 
 public class _1_DataTypes {
-    static int dataTypeSize(String str){
-        int value = 0;
-        switch(str){
-            case "Character":
-                value = Character.SIZE / 8;
-                break;
-            
-            case "Integer":
-                value = Integer.SIZE / 8;
-                break;
-
-            case "Float":
-                value = Float.SIZE / 8;
-                break;
-
-            case "Long":
-                value = Long.SIZE / 8;
-                break;
-
-            case "Double":
-                value = Double.SIZE / 8;
-                break;
-
+    public static int dataTypeSize(String str) {
+        switch(str.toLowerCase()){
+            case "character":
+                return Character.SIZE;
+            case "integer":
+                return Integer.SIZE;
+            case "long":
+                return Long.SIZE;
+            case "float":
+                return Float.SIZE;
+            case "double":
+                return Double.SIZE;
             default:
-                value = -1;
+                return -1;
         }
-        return value;
     }
 }
 
