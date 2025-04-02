d957796a9366a36a41607c55c34d8a4c40b36177
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-04-02 18:53:44 +0530

    Restructured the directories

---
+++ Striverz_A2Z_DSA/Step_1_Basics/Lec3/Test4.java
@@ -0,0 +1,23 @@
+import java.util.Collection;
+import java.util.Iterator;
+// import java.util.Set;
+import java.util.TreeSet;
+
+public class Test4 {
+    public static void main(String[] args) {
+        // Set<Integer> nums = new HashSet<Integer>();
+        Collection<Integer> nums = new TreeSet<Integer>();
+        nums.add(62); 
+        nums.add(54); 
+        nums.add(82); 
+        nums.add(21);
+
+        // for (int n : nums) {
+        //     System.out.println(n);
+        // }
+        Iterator<Integer> values = nums.iterator();
+        while(values.hasNext()){
+            System.out.println(values.next());
+        }
+    }
+}
